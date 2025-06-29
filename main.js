52eb052f6ba17a18ff09e3adc9439aeca02e8746
cngn <pruebagithubcngn@gmail.com>
2025-06-29 16:17:34 -0300

    Agregue functions add take multiply divide en main.js

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ const $buttonDivide = document.querySelector("#divide")
 
 //
 const $buttonEquals = document.querySelector("#equals")
+
+function add(a, b) {
+    const resultAdd = a + b
+    return resultAdd
+}
+
+function take(a, b) {
+    const resultTake = a - b
+    return resultTake
+}
+
+function multiply(a, b) {
+    const resultMultiply = a * b
+    return resultMultiply
+}
+
+function divide(a, b) {
+    const resultDivide = a / b
+    return resultDivide
+}
