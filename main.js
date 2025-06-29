fbc0ccc7031bed436a9ceb08e796c2e07de28d58
cngn <pruebagithubcngn@gmail.com>
2025-06-29 15:49:28 -0300

    Agregue elementos html en main.js y modificacion en index.html para tal fin

---
+++ main.js
@@ -0,0 +1,19 @@
+//                  elementos html
+
+// campo que el usuario completara
+const $inputUser = document.querySelector("#input-user")
+
+//
+const $buttonAdd = document.querySelector("#plus-add")
+
+//
+const $buttonTake = document.querySelector("#minus-take")
+
+//
+const $buttonMultiply = document.querySelector("#multiply-times")
+
+//
+const $buttonDivide = document.querySelector("#divide")
+
+//
+const $buttonEquals = document.querySelector("#equals")
