aa8c64e68f523d14c951fafccc1cc738c6876df1
cngn <pruebagithubcngn@gmail.com>
2025-06-29 21:19:45 -0300

    Aregle la declaracion de elementos html y la limpieza en  elimine los return de las funciones en main.js

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
 //                  declaracion de elementos html
 
 // campo que el usuario completara. input
-const $inputUserA = document.getElementById("#input-user-a")
+const $inputUserA = document.getElementById("input-user-a")
 
 // campo que el usuario completara. input
-const $inputUserB = document.getElementById("#input-user-b")
+const $inputUserB = document.getElementById("input-user-b")
 
 // campo donde se imprimira el resultado. p
-const $result = document.getElementById("#result")
+const $result = document.getElementById("result")
 
 // boton sumar
-const $buttonAdd = document.getElementById("#plus-add")
+const $buttonAdd = document.getElementById("plus-add")
 
 // boton sumar
-const $buttonTake = document.getElementById("#minus-take")
+const $buttonTake = document.getElementById("minus-take")
 
 // boton multiplicar
-const $buttonMultiply = document.getElementById("#multiply-times")
+const $buttonMultiply = document.getElementById("multiply-times")
 
 // boton dividir
-const $buttonDivide = document.getElementById("#divide")
+const $buttonDivide = document.getElementById("divide")
 
 // boton limpiar
-const $buttonClear = document.getElementById("#clear")
+const $buttonClear = document.getElementById("clear")
 
 
 //              funciones
 
 // funcion para limpiar input y parrafo
 function clear() {
+    // se limpia el primer campo de entrada
     $inputUserA.value = ""
+    // se limpia el segundo campo de entrada
     $inputUserB.value = ""
-    $result.value = ""
-    return
+    // se limpia el lugar en donde se muestra el resultado
+    $result.innerHTML = ""
 }
 
 // funcion para sumar valores
@@ -42,7 +44,6 @@ function add() {
     const resultAdd = aAdd + bAdd
     console.log(resultAdd)
     $result.textContent = resultAdd
-    return
 }
 
 // funcion para restar valores
@@ -52,7 +53,6 @@ function take() {
     const resultTake = aTake - bTake
     console.log(resultTake)
     $result.textContent = resultTake
-    return
 }
 
 // funcion para multiplicar valores
@@ -62,7 +62,6 @@ function multiply() {
     const resultMultiply = aMultiply * bMultiply
     console.log(resultMultiply)
     $result.textContent = resultMultiply
-    return
 }
 
 // funcion para dividir valores
@@ -72,7 +71,6 @@ function divide() {
     const resultDivide = aDivide / bDivide
     console.log(resultDivide)
     $result.textContent = resultDivide
-    return
 }
 
 
